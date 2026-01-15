9e372cf04bc3228b5b9790c9b1eb36244b517b9b
arytmw <54942957+arytmw@users.noreply.github.com>
2026-01-15 12:13:03 +0530

    Update customer_chat.py

--- checkpointing/customer_chat.py
+++ checkpointing/customer_chat.py
@@ -24,6 +24,11 @@ def poll_for_human_response(graph, config, poll_interval=2):
 
 
 
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from graph import build_graph
+from config import DB_URI, GRAPH_CONFIG
+
+
 def customer_chat():
     with MongoDBSaver.from_conn_string(DB_URI) as cp:
         graph = build_graph(cp)
@@ -35,6 +40,18 @@ def customer_chat():
                 "messages": [{"role": "user", "content": user_input}]
             }
 
+            interrupted = False
+
             for event in graph.stream(state, GRAPH_CONFIG, stream_mode="values"):
                 if "messages" in event:
                     event["messages"][-1].pretty_print()
+
+            # After execution, check if graph is waiting for resume
+            current_state = graph.get_state(config=GRAPH_CONFIG)
+
+            if current_state.next is not None:
+                interrupted = True
+
+            if interrupted:
+                poll_for_human_response(graph, GRAPH_CONFIG)
+
